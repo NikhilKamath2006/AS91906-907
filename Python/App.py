458244c1f45c15ec570a1a1e1d684196ae6e8cdb
NikhilKamath2006 <Nikhil.kamath@my.bdsc.school.nz>
2023-06-14 13:28:10 +1200

    Initial Menu

--- Python/App.py
+++ Python/App.py
@@ -1,9 +1,24 @@
 from tkinter import*
 
+
 root = Tk()
 root.geometry = ('500x500')
 
-print('hello')
+mainmenu = Menu
+
+def _quit():  
+   root.quit()  
+   root.destroy()  
+   exit()  
+#Create Menu   
+mainmenu=Menu(root)  
+root.config(menu=mainmenu)  
+#File Menu  
+main= Menu(mainmenu)  
+main.add_command(label="Exit", command=_quit)  
+mainmenu.add_separator(label="Options", menu= main)
+  
 
 
 
+root.mainloop()
