6415497c78cc69cc4a71c559e9f1883114a726b7
NikhilKamath2006 <Nikhil.kamath@my.bdsc.school.nz>
2023-07-30 19:28:20 +1200

    Final program

--- Python/App.py
+++ Python/App.py
@@ -1,11 +1,12 @@
 from tkinter import*
 from tkinter import messagebox
 import datetime
-from pip import *
+from tkinter import ttk
 root = Tk()
-
+root.resizable(0,0)
 main_menu = Menu(root)
-root.config(menu = main_menu,bg="#40e0d0")
+root.config(menu = main_menu,bg="#1B60F4")
+root.title('Smart Calculator')
 
 class   Calculator:
     
@@ -15,76 +16,76 @@ class   Calculator:
         #Then when equal is clicked the calculate function uses the data from display function
         #to print the final answer
         self.solve =''
-        self.live_display = Text(calc_frame,height='2',width='20',bg='#cccccc',font=('xenara','15'))
-        self.live_display.grid(row=0,columnspan=5,)
+        self.live_display = Text(calc_frame,height='2',width='20',bg='#cccccc',font=('xenara','15'),)
+        self.live_display.grid(row=0,columnspan=5,pady='2',padx='2')
         
         self.button0= Button(calc_frame,text="0",command = lambda:self.display(0),
-                        height = '2', width = '2',font='xenara',bg='#8aecff') 
-        self.button0.grid(row=1,column=0)
+                        height = '1', width = '2',font='xenara',bg='#f4af1b') 
+        self.button0.grid(row=1,column=0,pady='2')
 
-        self.button1= Button(calc_frame,text="1",command = lambda:self.display(1),bg='#8aecff',
-                        height = '2', width = '2',font='xenara') 
-        self.button1.grid(row=1,column=1)
+        self.button1= Button(calc_frame,text="1",command = lambda:self.display(1),bg='#f4af1b',
+                        height = '1', width = '2',font='xenara') 
+        self.button1.grid(row=1,column=1,pady='2')
 
-        self.button2= Button(calc_frame,text="2",command = lambda:self.display(2),bg='#8aecff',
-                        height = '2', width = '2',font='xenara') 
-        self.button2.grid(row=1,column=2)
+        self.button2= Button(calc_frame,text="2",command = lambda:self.display(2),bg='#f4af1b',
+                        height = '1', width = '2',font='xenara') 
+        self.button2.grid(row=1,column=2,pady='2')
 
-        self.button3= Button(calc_frame,text="3",command = lambda:self.display(3),bg='#8aecff',
-                        height = '2', width = '2',font='xenara') 
-        self.button3.grid(row=1,column=3)
+        self.button3= Button(calc_frame,text="3",command = lambda:self.display(3),bg='#f4af1b',
+                        height = '1', width = '2',font='xenara') 
+        self.button3.grid(row=1,column=3,pady='2')
 
-        self.button4= Button(calc_frame,text="4",command = lambda:self.display(4),bg='#8aecff',
-                        height = '2', width = '2',font='xenara') 
-        self.button4.grid(row=1,column=4)
+        self.button4= Button(calc_frame,text="4",command = lambda:self.display(4),bg='#f4af1b',
+                        height = '1', width = '2',font='xenara') 
+        self.button4.grid(row=1,column=4,pady='2')
 
-        self.button5= Button(calc_frame,text="5",command = lambda:self.display(5),bg='#8aecff',
-                        height = '2', width = '2',font='xenara') 
-        self.button5.grid(row=2,column=0)
+        self.button5= Button(calc_frame,text="5",command = lambda:self.display(5),bg='#f4af1b',
+                        height = '1', width = '2',font='xenara') 
+        self.button5.grid(row=2,column=0,pady='2')
 
-        self.button6= Button(calc_frame,text="6",command = lambda:self.display(6),bg='#8aecff',
-                        height = '2', width = '2',font='xenara') 
-        self.button6.grid(row=2,column=1)
+        self.button6= Button(calc_frame,text="6",command = lambda:self.display(6),bg='#f4af1b',
+                        height = '1', width = '2',font='xenara') 
+        self.button6.grid(row=2,column=1,pady='2')
 
-        self.button7= Button(calc_frame,text="7",command = lambda:self.display(7),bg='#8aecff',
-                        height = '2', width = '2',font='xenara') 
-        self.button7.grid(row=2,column=2)
+        self.button7= Button(calc_frame,text="7",command = lambda:self.display(7),bg='#f4af1b',
+                        height = '1', width = '2',font='xenara') 
+        self.button7.grid(row=2,column=2,pady='2')
 
-        self.button8= Button(calc_frame,text="8",command = lambda:self.display(8),bg='#8aecff',
-                        height = '2', width = '2',font='xenara') 
-        self.button8.grid(row=2,column=3)
+        self.button8= Button(calc_frame,text="8",command = lambda:self.display(8),bg='#f4af1b',
+                        height = '1', width = '2',font='xenara') 
+        self.button8.grid(row=2,column=3,pady='2')
 
-        self.button9= Button(calc_frame,text="9",command = lambda:self.display(9),bg='#8aecff',
-                        height = '2', width = '2',font='xenara')
-        self.button9.grid(row=2,column=4)
+        self.button9= Button(calc_frame,text="9",command = lambda:self.display(9),bg='#f4af1b',
+                        height = '1', width = '2',font='xenara')
+        self.button9.grid(row=2,column=4,pady='2')
 
-        self.subtract_button = Button(calc_frame,text='-',command=lambda:self.display("-"),bg='#8aecff',
-                                height = '2', width = '2',font='xenara')
+        self.subtract_button = Button(calc_frame,text='-',command=lambda:self.display("-"),bg='#f4af1b',
+                                height = '1', width = '2',font='xenara')
         self.subtract_button.grid(row=3,column = 0)
 
-        self.add_button = Button(calc_frame,text='+',command=lambda:self.display("+"),bg='#8aecff',
-                            height = '2', width = '2',font='xenara')
+        self.add_button = Button(calc_frame,text='+',command=lambda:self.display("+"),bg='#f4af1b',
+                            height = '1', width = '2',font='xenara')
         self.add_button.grid(row=3,column=1)
 
-        self.divide_button = Button(calc_frame,text ='/',command=lambda:self.display("/"),bg='#8aecff',
-                            height = '2', width = '2',font='xenara')
-        self.divide_button.grid(row=3,column=2)
+        self.divide_button = Button(calc_frame,text ='/',command=lambda:self.display("/"),bg='#f4af1b',
+                            height = '1', width = '2',font='xenara')
+        self.divide_button.grid(row=3,column=2,pady='2')
 
-        self.multiply_button = Button(calc_frame,text='x',command=lambda:self.display("*"),bg='#8aecff',
-                                height = '2', width = '2',font='xenara')
-        self.multiply_button.grid(row=3,column=3)
+        self.multiply_button = Button(calc_frame,text='x',command=lambda:self.display("*"),bg='#f4af1b',
+                                height = '1', width = '2',font='xenara')
+        self.multiply_button.grid(row=3,column=3,pady='2')
 
-        self.decimal_button = Button(calc_frame,text='.',command=lambda:self.display("."),bg='#8aecff',
-                                height = '2', width = '2',font='xenara')
-        self.decimal_button.grid(row=4,column=3)
+        self.decimal_button = Button(calc_frame,text='.',command=lambda:self.display("."),bg='#f4af1b',
+                                height = '1', width = '2',font='xenara')
+        self.decimal_button.grid(row=3,column=4,pady='2')
         
         self.clear_button = Button(calc_frame,text='C',command= self.clear,
-                            height = '2', width = '2',bg='#FF0000',font='xenara')
-        self.clear_button.grid(row=3,column=4)
+                            height = '1', width = '2',bg='#FF0000',font='xenara')
+        self.clear_button.grid(row=4,column=4,pady='2')
 
         self.equal_button = Button(calc_frame,text='=',command= self.calculate,
-                            height = '2', width = '2',background="#86DC3D",font='xenara')
-        self.equal_button.grid(row=4,column=2)
+                            height = '1', width = '15',background="#86DC3D",font='xenara')
+        self.equal_button.grid(row=4,columnspan=4,pady='2')
             
     def display(self,user_input):
         self.solve += str(user_input)
@@ -99,16 +100,20 @@ class   Calculator:
             self.solve = str(eval(self.solve))
             self.live_display.delete(1.0,'end')
             self.live_display.insert(1.0,self.solve)
-        except:
-            self.clear()
+        except ZeroDivisionError:
             self.live_display.insert(1.0,"Error, please check your entry")
+            self.live_display.delete(1.0,'end')
+            messagebox.showerror('Error,Please check entry')
+        except:
+            self.live_display.insert(1.0,"Error, please check your entry")
+            messagebox.showerror('Error','Please check entry')
+            self.live_display.delete(1.0,'end')
             
     #This function is for the clear button on the calculator, if clicked the display box clears.
     #A messagebox popup will allow user to confirm their choice
     def clear(self): 
         if messagebox.askyesno(message="This will clear your calculation are you sure?") == True:
-            global solve
-            solve=""
+            self.solve =''
             self.live_display.delete(1.0,'end')
         else:
             pass
@@ -116,17 +121,17 @@ class   Calculator:
 class Tax:
     def __init__(self,tax_frame):
         #initiating the tax class
-        self.tax_label = Label(tax_frame,text='What is your yearly income?',bg="#ffdb58",font='xenara',borderwidth=5,relief='groove')
-        self.tax_label.grid()
+        self.tax_label = Label(tax_frame,text='What is your yearly income?',bg="#6dd47e",font='xenara',borderwidth=5,relief='groove')
+        self.tax_label.grid(pady='2',padx='2')
 
-        self.taxed_label = Label(tax_frame,bg="#40e0d0",font='xenara')
-        self.taxed_label.grid()
+        self.taxed_label = Label(tax_frame,bg="#1b60f4",font='xenara')
+        self.taxed_label.grid(padx='2',pady='2')
         
         self.salary = Entry(tax_frame)
-        self.salary.grid()
+        self.salary.grid(padx='2',pady='2')
         
-        self.tax_button = Button(tax_frame,text='Tax Me!',command = self.calculate_tax ,bg="#ffdb58",font='xenara')
-        self.tax_button.grid()
+        self.tax_button = Button(tax_frame,text='Tax Me!',command = self.calculate_tax ,bg="#6dd47e",font='xenara',pady='2')
+        self.tax_button.grid(pady='2')
     
     def calculate_tax(self):
         #checks if the input is valid
@@ -135,7 +140,7 @@ class Tax:
             int_salary = float(chk_salary)
         except ValueError:
             messagebox.showerror('Error','Please check entry')
-            self.taxed_label.config(text="Error,try again")
+            self.taxed_label.config(text="Error,try again",fg='white')
         #calculates the tax and these if statments are for the diffrent tax brackets
         int_salary = float(self.salary.get())
         if 0< int_salary <= (14000):
@@ -164,51 +169,54 @@ class Tax:
 class Date:
     def __init__(self,days_frame):
         #range of options
-        day_options = range(32)
-        month_options = range(13)
-        year_options = range(100)
+        day_options = range(1,32)
+        month_options = range(1,13)
+        year_options = range(1900,2024)
+        
         
         #converting options from optionmenu into integers so it can be used with datetime
         self.day1_int = IntVar()
-        self.day1_int.set(day_options[0])
+        self.day1_int.set(day_options[1])
 
         self.day2_int = IntVar()
-        self.day2_int.set(day_options[0])
+        self.day2_int.set(day_options[1])
 
         self.month1_int = IntVar()
-        self.month1_int.set(month_options[0])
+        self.month1_int.set(month_options[1])
 
         self.month2_int = IntVar()
-        self.month2_int.set(month_options[0])
+        self.month2_int.set(month_options[1])
 
         self.year1_int = IntVar()
-        self.year1_int.set(year_options[0])
+        self.year1_int.set(year_options[1])
 
         self.year2_int = IntVar()
-        self.year2_int.set(year_options[0])
+        self.year2_int.set(year_options[1])
        
         #widgets for the date to days feature
-        self.day1 = OptionMenu(days_frame,self.day1_int,*day_options)
-        self.day1.grid(row = 1, column = 0)
-        self.day2 = OptionMenu(days_frame,self.day2_int,*day_options)
-        self.day2.grid(row = 2, column = 0)
+        self.format_label = Label(days_frame,text='Please use DD/MM/YY format',bg='#f4421b',font='xenera',relief='sunken')
+        self.format_label.grid(row=1,columnspan=4,pady=2,padx=2)
+        self.day1 = ttk.OptionMenu(days_frame,self.day1_int,*day_options)
+        self.day1.grid(row = 2, column = 0,pady=2,padx=2)
+        self.day2 = ttk.OptionMenu(days_frame,self.day2_int,*day_options)
+        self.day2.grid(row = 3, column = 0,pady=2,padx=2)
         
-        self.month1 = OptionMenu(days_frame,self.month1_int,*month_options)
-        self.month1.grid(row = 1, column=1)
-        self.month2 = OptionMenu(days_frame,self.month2_int,*month_options)
-        self.month2.grid(row =2 , column = 1)
+        self.month1 = ttk.OptionMenu(days_frame,self.month1_int,*month_options)
+        self.month1.grid(row = 2, column=1,pady=2,padx=2)
+        self.month2 = ttk.OptionMenu(days_frame,self.month2_int,*month_options)
+        self.month2.grid(row =3 , column = 1,pady=2,padx=2)
 
-        self.year1 = OptionMenu(days_frame,self.year1_int,*year_options)
-        self.year1.grid(row = 1, column = 2)
-        self.year2 = OptionMenu(days_frame,self.year2_int,*year_options)
-        self.year2.grid(row = 2, column = 2)
+        self.year1 = ttk.OptionMenu(days_frame,self.year1_int,*year_options)
+        self.year1.grid(row = 2, column = 2,pady=2,padx=2)
+        self.year2 = ttk.OptionMenu(days_frame,self.year2_int,*year_options)
+        self.year2.grid(row = 3, column = 2,pady=2,padx=2)
         
-        self.question = Label(days_frame,text='Calculate the days between dates!',height='3',pady=10)
-        self.question.grid(row = 0,columnspan= 5)
-        self.days = Label(days_frame)
-        self.days.grid(row = 4,column = 1)
-        self.find = Button(days_frame,text='Calculate',command = self.find_days)
-        self.find.grid(row=3,column=1)
+        self.question = Label(days_frame,text='Calculate the days between dates!',height='2',pady=10,bg='#f4421b',font='xenera',relief='sunken')
+        self.question.grid(row =0,columnspan= 5,pady=2,padx=2)
+        self.days = Label(days_frame,bg='#1b60f4')
+        self.days.grid(row = 5,column = 1,pady=2,padx=2)
+        self.find = ttk.Button(days_frame,text='Calculate',command = self.find_days)
+        self.find.grid(row=4,column=1,pady=2,padx=2)
 
     #using datetime module to convert optionmenu into integer then convert to date
     def find_days(self):
@@ -224,25 +232,31 @@ class Date:
 #Class for the welcome page, the buttons will guide user to diffrent parts of app
 class Welcome():
     def __init__(self,welcome_frame):
-        self.title = Label(welcome_frame,text='Welcome to the Smart Calculator')
-        self.title.grid()
 
-        self.tax_feature = Button(welcome_frame,text='Tax',command = tax)
-        self.tax_feature.grid()
+        self.title = Label(welcome_frame,text='Welcome to the Smart Calculator',bg='#1bcdf4',font=('xenara','18'))
+        self.title.grid(row=0,columnspan=4,padx=2,pady=2)
 
-        self.calc_feature = Button(welcome_frame,text ='Calculator',command = calculator)
-        self.calc_feature.grid()
+        self.info =  Label(welcome_frame,text='Calculate numbers,tax or days!, Click any button to begin.',bg='#1bcdf4',font='xenara')
+        self.info.grid(row=1,columnspan=4)
 
-        self.date_feature = Button(welcome_frame,text='Date to Days',command = days_date)
-        self.date_feature.grid()
+        self.tax_feature = Button(welcome_frame,command = tax,text='Tax',height=5,width=10,bg='#1bcdf4',font='xenara')
+        self.tax_feature.grid(row=3,column=0,padx=2,pady=2)
+
+        self.calc_feature = Button(welcome_frame,command = calculator,text='Calculator',height=5,width=10,bg='#1bcdf4',font='xenara')
+        self.calc_feature.grid(row=3,column=1,padx=2,pady=2)
+
+        self.date_feature = Button(welcome_frame,command = days_date,text='Date To Days',height=5,bg='#1bcdf4',font='xenara')
+        self.date_feature.grid(row=3,column=2,padx=2,pady=2)
+
+     
 
 
 
 #Create frames for each section
-calc_frame = Frame(root, bg="#40e0d0")
-tax_frame = Frame(root, bg="#40e0d0")
-days_frame = Frame(root, bg="#40e0d0")
-welcome_frame = Frame(root,bg='#40e0d0')
+calc_frame = Frame(root, bg="#1b60f4")
+tax_frame = Frame(root, bg="#1b60f4")
+days_frame = Frame(root, bg="#1b60f4")
+welcome_frame = Frame(root,bg="#1B60F4")
 
 def clear_frame():
     for widget in tax_frame.winfo_children():
